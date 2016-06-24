69676c083acf9166975595368cd8a90ba38a05d7
Alexey Dobrov <mail@web-master-pro.ru>
2016-06-24 17:30:11 +0300

    Autoplay for portfolio videos

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -287,7 +287,30 @@ $(document).ready(function(){
         fixedContentPos: true
     });
 
-    $(".popup-portfolio__video").fitVids();
+    $('.js-popup-portfolio').magnificPopup({
+        type:'inline',
+        midClick: true,
+        removalDelay: 500,
+        mainClass: 'mfp-zoom-in',
+        overflowY: 'scroll',
+        fixedContentPos: true,
+        callbacks: {
+            open: function() {
+                var popupBox = $.magnificPopup.instance.content;
+                    videoDiv = $(popupBox).children(".popup-portfolio__video"),
+                    videoSrc = videoDiv.attr("data-src"),
+                    newEl = '<iframe src="' + videoSrc + '" width="640" height="360" allowfullscreen></iframe>';
+                    videoDiv.append(newEl);
+                    $(".popup-portfolio__video").fitVids();
+            },
+            close: function() {
+                var popupBox = $.magnificPopup.instance.content;
+                    videoDiv = $(popupBox).children(".popup-portfolio__video"),
+                    videoDiv.empty();;
+            }
+          }
+    });
+
 
     $(".js-close-button").click(function (e) {
         e.preventDefault();
